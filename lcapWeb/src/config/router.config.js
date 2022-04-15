7467d1085cee0877522937aff84f2b2158143a69
Ethan.Du <ethan.dwq@foxmail.com>
2022-04-15 18:56:46 +0800

    Add: add lcapWeb lcapCodeServer lcapWww

--- lcapWeb/src/config/router.config.js
+++ lcapWeb/src/config/router.config.js
@@ -21,12 +21,6 @@ module.exports = {
       routes: [
         {
           icon: "pie-chart",
-          name: "工作台",
-          path: "/dashboard",
-          component: "@/pages/Dashboard"
-        },
-        {
-          icon: "pie-chart",
           name: "应用创建",
           path: "/app",
           component: "@/pages/App",
@@ -70,25 +64,6 @@ module.exports = {
         },
         {
           icon: "pie-chart",
-          name: "模板库",
-          path: "/template",
-          component: "@/pages/Template",
-          routes: [
-            {
-              name: "应用模板库",
-              path: "/template/apply-template",
-              component: "@/pages/Template/TemplateApp",
-            },
-            {
-              name: "组件库",
-              path: "/template/library-template",
-              component: "@/pages/Template/TemplateComponent",
-            },
-            { from: "/template", to: "/template/apply-template" },
-          ],
-        },
-        {
-          icon: "pie-chart",
           name: "用户管理",
           path: "/user",
           component: "@/pages/User",
@@ -106,26 +81,7 @@ module.exports = {
             { from: "/user", to: "/user/user-manage" },
           ],
         },
-        {
-          icon: "pie-chart",
-          name: "API应用服务层",
-          path: "/api",
-          component: "@/pages/Api",
-          routes: [
-            {
-              name: "API列表",
-              path: "/api/api-list",
-              component: "@/pages/Api/ApiList/index.js",
-            },
-            {
-              name: "应用管理",
-              path: "/api/api-manage",
-              component: "@/pages/Api/ApiManage/index.js",
-            },
-            { from: "/api", to: "/api/api-list" },
-          ],
-        },
-        { from: "/", to: "/dashboard" },
+        { from: "/", to: "/app" },
       ],
     },
   ],
---
+++ lcapWeb/themes/light.js
@@ -0,0 +1,3 @@
+module.exports = {
+  "@primary-color": "red",
+};
