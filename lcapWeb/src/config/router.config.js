72615bc0931db2451d9192c6998a1f6863a5ac65
tiger.wang <1791066221@qq.com>
2022-05-18 19:10:09 +0800

    定时同步20220518191008

--- lcapWeb/src/config/router.config.js
+++ lcapWeb/src/config/router.config.js
@@ -5,7 +5,7 @@
  * @LastEditors: tiger.wang
  * @LastEditTime: 2022-05-18 19:05:03
  */
-// 2022-05-18 7:05
+// 2022-05-18 7:06
 module.exports = {
   loadingComponent: "@/components/Loading", // 路由按需加载 loading组件
   noAuthShow: "@/components/NoAuth", // 无权限展示效果
