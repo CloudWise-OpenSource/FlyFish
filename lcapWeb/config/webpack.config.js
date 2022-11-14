2b38e81b52298bf7ea62f9bce483db5dac757263
jincheny <1776758048@qq.com>
2022-11-14 16:22:23 +0800

    Fixed 连接地址placeholder提示文案修改

--- lcapWeb/config/webpack.config.js
+++ lcapWeb/config/webpack.config.js
@@ -7,14 +7,7 @@
  */
 const path = require('path');
 const modifyVars = require('./themes/dark.js');
-
-const fs = require('fs');
-
 // 详细扩展配置参考  https://www.npmjs.com/package/@chaoswise/scaffold
-
-const args = process.argv;
-
-const isApi = args[3] === 'api';
 
 const config = {
   debugIe: false, // 是否在ie下调试(关闭热更新)
@@ -69,29 +62,29 @@ const config = {
   devServer: (config) => {
     config.port = 8000;
     config.proxy = {
-      "/api": {
-        target: "http://10.2.3.56:7001",
-        changeOrigin:true,
+      '/api': {
+        target: 'http://10.2.2.249:7001',
+        changeOrigin: true,
         pathRewrite: {
-          "^/api": "",
+          '^/api': '',
         },
-        onProxyReq(proxyReq, req, res) {
+        onProxyReq(proxyReq) {
           proxyReq.setHeader(
-              'cookie',
-              'locale=en-us; FLY_FISH_V2.0=CM0gjxPflfzqKGborcqydz1b7/3SJIfWUYx2nUSauPcGgiRI5zEX1uC4qOAqFyysvESpjN/mu92jsHT5K21vy4dgfi5TbvNsG+xhkMQRu0lgCvuGmS50WOku/ROZmiRmK1YigwUG0U6kHoZVhbW9vrouG5rOVY/iabCdw9LSaXfq4udNhZ3qyFOqex0K/CRT',
+            'cookie',
+            'locale=en-us; FLY_FISH_V2.0=jNP1eG7jeiXED7Go6d5P4YC8qGI5kJO8gYFT3VLlg8GX3XhTme7n3OhABxVbGjJNlTIc/EW06c8o+D4/bE1YaepqEiqb//d/jD4spssRx2z3T2SI5lWpPZPkJjhE9s4fHt3HPvzgkNIa6rO/X5om8nPznidjnpT9LDq7/NB1noS3r4fNt1CbzAKqlTugL7eC'
           );
         },
       },
-      "/lcap-data-server": {
-        target: "http://10.2.3.56:7002",
-        changeOrigin:true,
+      '/lcap-data-server': {
+        target: 'http://10.2.2.249:18532',
+        changeOrigin: true,
         pathRewrite: {
-          "^/lcap-data-server": "",
+          '^/lcap-data-server': '',
         },
-        onProxyReq(proxyReq, req, res) {
+        onProxyReq(proxyReq) {
           proxyReq.setHeader(
-              'cookie',
-              'locale=en-us; FLY_FISH_V2.0=CM0gjxPflfzqKGborcqydz1b7/3SJIfWUYx2nUSauPcGgiRI5zEX1uC4qOAqFyysvESpjN/mu92jsHT5K21vy4dgfi5TbvNsG+xhkMQRu0lgCvuGmS50WOku/ROZmiRmK1YigwUG0U6kHoZVhbW9vrouG5rOVY/iabCdw9LSaXfq4udNhZ3qyFOqex0K/CRT',
+            'cookie',
+            'locale=en-us; FLY_FISH_V2.0=jNP1eG7jeiXED7Go6d5P4YC8qGI5kJO8gYFT3VLlg8GX3XhTme7n3OhABxVbGjJNlTIc/EW06c8o+D4/bE1YaepqEiqb//d/jD4spssRx2z3T2SI5lWpPZPkJjhE9s4fHt3HPvzgkNIa6rO/X5om8nPznidjnpT9LDq7/NB1noS3r4fNt1CbzAKqlTugL7eC'
           );
         },
       },
